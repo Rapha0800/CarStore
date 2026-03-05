15bd3fc85315405bb4471532e229c26a938c10c2
Raphael Mazzieri <raphaelleite0800@gmail.com>
2026-03-05 01:00:52 -0300

    Adicionando descricoes para os codigos em json

--- src/main/java/br/com/carstore/servlet/CreateCarServlet.java
+++ src/main/java/br/com/carstore/servlet/CreateCarServlet.java
@@ -12,15 +12,19 @@ public class CreateCarServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        /*Ira pegar o nome do carro pelo formulario*/
         String carName = req.getParameter("car-name");
 
+        /*Ira printar o nome do carro no terminal*/
         System.out.println(carName);
 
+        /*Ira dizer que a resposta sera em json*/
         resp.setContentType("application/json");
 
+        /*criando o json que ira receber o valor digitado*/
         String json = "{ \"carName\": \"" + carName + "\"}";
 
+        /*Ira enviar o json*/
         resp.getWriter().write(json);
 
 
